90181e27c6ef7f4293396f7f59d69af3e0c16524
AliceChen8 <162983670+AliceChen8@users.noreply.github.com>
2024-03-11 13:05:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit as st
-name = st.text_imput("Your name")
-st.write("Hello"+name)
+imput=("lapin")
+list_possibilities=["rabbit","race","rice","rhyme"]
+for: in rage(len(list_possibilities))
+st.button(list_possibilities[i])
